5ba465722e1b4dd71116ae081ae2da7df9676150
Rahul Parmar <rahulparmar.n@gmail.com>
2019-10-23 01:12:42 +0530

    Updates

---
+++ precache-manifest.92fe8f4cd77ef6c89d6a0ab60f2ddb62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80742821918200893200aa336a862f33",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "41fc3114da702c7d8a25",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "205077743e1bc62837b9",
+    "url": "/portfolio/static/js/2.c67c5a6f.chunk.js"
+  },
+  {
+    "revision": "41fc3114da702c7d8a25",
+    "url": "/portfolio/static/js/main.9f24093f.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
